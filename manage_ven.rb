9925178ecf7047d3c21a58fd52440a844dfd78e9
Anthony DuPont <tony@nebland.com>
2017-10-18 13:10:41 -0400

    update "manage_ven" functions for changes in vtn api

--- manage_ven.rb
+++ manage_ven.rb
@@ -2,34 +2,36 @@ module Api
 
   module ManageVen
 
-    def self.ven_id(request_body)
-      body_hash = ActiveSupport::JSON.decode(request_body.to_s) # Argument should be JSON as a string
+    SCHEMA = {
+        ven_id: { validate: false },
+        status: { validate: false },
+        last_comm_time: { validate: false }
+    }
+    
+    #####################################################################
 
-      ven = Ven.find_by_name(body_hash['ven'])
+    def self.ven_id(request_body)
+      ven = Ven.find_by_name(request_body['ven'])
 
       result_hash = { "ven_id" => ven.ven_id }
 
       return result_hash
     end
 
-    # # # # # # # # # # # # # # # # # # # # # # # #
+    #####################################################################
 
     def self.status(request_body)
-      body_hash = ActiveSupport::JSON.decode(request_body.to_s) # Argument should be JSON as a string
-
-      ven = Ven.find_by_ven_id(body_hash['ven_id'])
+      ven = Ven.find_by_ven_id(request_body['ven_id'])
 
       result_hash = { "status" => ven.status }
 
       return result_hash
     end
 
-    # # # # # # # # # # # # # # # # # # # # # # # #
+    #####################################################################
 
     def self.last_comm_time(request_body)
-      body_hash = ActiveSupport::JSON.decode(request_body.to_s) # Argument should be JSON as a string
-
-      ven = Ven.find_by_ven_id(body_hash['ven_id'])
+      ven = Ven.find_by_ven_id(request_body['ven_id'])
 
       result_hash = { "last_comm_time" => ven.last_comm_at }
 
